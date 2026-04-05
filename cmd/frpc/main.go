06c6bfb94c3bff0b500c7c2d258ebdf9c068c92d
xxl6097 <263996097@qq.com>
2026-04-05 21:30:58 +0800

    【v0.5.15】by uuxia 2026-04-05 21:30:58

--- cmd/frpc/main.go
+++ cmd/frpc/main.go
@@ -2,7 +2,17 @@ package main
 
 import (
 	"github.com/xxl6097/go-frp-panel/cmd/frpc/service"
+	"github.com/xxl6097/go-frp-panel/pkg"
+	"github.com/xxl6097/go-frp-panel/pkg/utils"
 )
+
+func init() {
+	if utils.IsMacOs() {
+		pkg.AppName = "acfrpc"
+		pkg.DisplayName = "acfrpc"
+		pkg.Description = "acfrpc"
+	}
+}
 
 //go:generate goversioninfo -icon=resource/icon.ico -manifest=resource/goversioninfo.exe.manifest
 func main() {
